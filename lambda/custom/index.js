491e5cb24b003d303bba3cda7bfa8df9fd8b5caf
simon <psimoncobb@gmail.com>
2017-12-05 09:49:03 +0000

    fixed array shuffle

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -22,28 +22,45 @@ var handlers = {
     'LaunchRequest': function () {
         init();
         //this.repsonse.speak("hello there");
-        this.response.speak("Hiya! Welcome to the music quiz. I'll play you a few seconds of a tune and then ask you what it is. Here comes the first one. " + getQuestion()).listen("what do you think? is it a or b?");
+        this.response.speak("Hiya! Think you know your chart music? Let' see... I'll play you a few seconds of a tune and then ask you what it is. Here comes the first one. " + getQuestion()).listen("what do you think? Say 1, or 2");
         this.emit(':responseReady');
     },
     'QuestionIntent': function () {
         this.response.speak("hello")
         this.emit(":responseReady");
     },
+
+    'RepeatIntent': function (){
+        console.log("inside repeat intent. Is this where it all ends?");
+        this.response.speak(getQuestion()).listen("what do you think? Say 1, or 2");
+        this.emit(':responseReady');
+    },
     'AnswerIntent': function () {
         console.log("Answer.value = " + this.event.request.intent.slots.Answer.value);
-
+        var feedback;
         //var name = this.event.request.intent.slots.name.value;
+        console.log("about to check answer....");
         if(checkAnswer(this.event.request.intent.slots.Answer.value))
         {
-          this.response.speak("YES! that's the right answer");
+          score ++;
+          feedback = "YES! that's the right answer.";
+        }else{
+          console.log("questionIndex: " + questionIndex);
+          feedback = "Unlucky. The correct answer was: " + questions[questionIndex][questions[questionIndex].correct];
+        }
+
+        questionIndex++
+        if(questionIndex >= questions.length)
+        {
+          this.response.speak(feedback + getScore());
           this.emit(":responseReady");
         }else{
-          this.response.speak("Unlucky. The correct answer was: " + questions[questionIndex].correct);
+          this.response.speak(feedback + ". Next question coming up. " + getQuestion()).listen("what do you think? Say 1, or 2");
           this.emit(":responseReady");
         }
-        //this.response.speak(getQuestion()).listen("what do you think? is it a or b?");
-      //  this.emit(":responseReady");
-        //this.emit('QuestionIntent');
+    },
+    'FinalScoreIntent': function (){
+        this.emit(":responseReady");
     },
     'SessionEndedRequest' : function() {
         console.log('Session ended with reason: ' + this.event.request.reason);
@@ -68,15 +85,23 @@ var handlers = {
 };
 
 const init = () =>{
-
   questionIndex = 0;
   score = 0;
-  let allQuestions = questionData.tunes;
-  while(allQuestions.length > 0){
-    let randomIndex = Math.floor(Math.random() * allQuestions.length);
-    questions.push(allQuestions[randomIndex]);
-    allQuestions.splice(randomIndex, 1);
+  var currentIndex = questionData.tunes.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = questionData.tunes[currentIndex];
+    questionData.tunes[currentIndex] = questionData.tunes[randomIndex];
+    questionData.tunes[randomIndex] = temporaryValue;
   }
+  questions = questionData.tunes;
 }
 
 const getQuestion = () => {
@@ -85,7 +110,7 @@ const getQuestion = () => {
     init();
   }
   let currentQuestion = questions[questionIndex];
-  let questionText = 'What tune is this? <audio src="'+ rootURL + currentQuestion.src + '"/> Is it "a" ' + currentQuestion.a + '. Or is it "b", ' + currentQuestion.b + '. What do you think? Is it "a", or is it "b"?';
+  let questionText = 'What tune is this? <audio src="'+ rootURL + currentQuestion.src + '"/> Is it, 1: ' + currentQuestion["1"] + '. Or is it, 2: ' + currentQuestion["2"] + '. What do you think? Is it 1, or is it 2?';
   return questionText;
 }
 
@@ -94,9 +119,14 @@ const checkAnswer = (answer) => {
   {
     init();
   }
-  console.log("Answer.value = " + answer);
   if(answer ===  questions[questionIndex].correct){
     return true;
   }
   return false;
+}
+
+const getScore = () =>
+{
+    let scoreText = " ... and that's the end of the quiz! I'll just count up the score. You scored: " + score + " out of " + questions.length + ". Thanks for playing! See you again soon!";
+    return scoreText;
 }
--- lambda/custom/quiz_config.js
+++ lambda/custom/quiz_config.js
@@ -3,10 +3,23 @@ const questionData = {
   [
     {
       "src": "1.mp3",
-      "a": "camila cabello havana featuring young thug ",
-      "b": "slade, but with Roy Wood off Wizzard",
-      "correct": "a"
+      "1": "camila cabello. havana. featuring young thug",
+      "2": "big shaq. Man's not Hot",
+      "correct": "1"
+    },
+    {
+      "src": "2.mp3",
+      "1": "Mabel. Finders Keepers. featuring Kojo Funds",
+      "2": "Rita Ora. Anywhere",
+      "correct": "2"
+    },
+    {
+      "src": "3.mp3",
+      "1": "Marshmello. Silence. Featuring Khalid",
+      "2": "Post Malone. Rock Star. Featuring 21 Savage",
+      "correct": "1"
     }
+
   ]
 }
 
